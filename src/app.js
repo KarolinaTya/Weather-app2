64f63f87d619a208e97a407e84dea0d492773ce2
KarolinaTya <80701688+KarolinaTya@users.noreply.github.com>
2022-07-14 22:56:02 +0100

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -61,10 +61,17 @@ function displayTemperature(response) {
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-let apiKey = "01619371674201e91b5c825d8719047b";
-let city = "New york";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
-
+function search(city) {
+  let apiKey = "01619371674201e91b5c825d8719047b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
 search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);

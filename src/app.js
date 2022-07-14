6eab480a67c11588620bfb6ba41f5d9fca051fa9
KarolinaTya <80701688+KarolinaTya@users.noreply.github.com>
2022-07-14 22:09:30 +0100

    Added relevant icon

--- src/app.js
+++ src/app.js
@@ -53,12 +53,17 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "01619371674201e91b5c825d8719047b";
+let city = "New york";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 axios.get(apiUrl).then(displayTemperature);
 
